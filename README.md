4183fce99cc0329faf31a0fae72044e909d5e30a
R.G. Wood <ric@grislyeye.com>
2024-04-05 18:39:00 +0100

    Add support for tables with dice rolls

--- README.md
+++ README.md
@@ -37,6 +37,40 @@ Simple, one-column table (elements are selected at random with equal weight):
 </vellum-random-table>
 ```
 
+Two-column table where items are selected by a dice roll:
+
+```html
+<vellum-random-table select="#result">
+  <caption>
+    Random Encounters
+  </caption>
+  <table>
+    <thead>
+      <tr>
+        <th>d6</th>
+        <th>Encounter</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1-3</td>
+        <td>1 wolf</td>
+      </tr>
+      <tr>
+        <td>4-5</td>
+        <td>2 goblins</td>
+      </tr>
+      <tr>
+        <td>6</td>
+        <td>dragon</td>
+      </tr>
+    </tbody>
+  </table>
+  <button>Roll</button>
+  <input id="result" type="text" />
+</vellum-random-table>
+```
+
 ## Installation
 
 Install via [npm](https://www.npmjs.com/package/@daviddarnes/component-name): `npm i vellum-random-table -S`.
--- src/vellum-random-table.ts
+++ src/vellum-random-table.ts
@@ -1,5 +1,12 @@
 import { LitElement, css, html } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
+import { Die } from './dice'
+import { Range, parseRange } from './range'
+
+enum TableMode {
+  FirstColumn,
+  TwoColumn,
+}
 
 @customElement('vellum-random-table')
 export class VellumRandomTable extends LitElement {
@@ -18,6 +25,14 @@ export class VellumRandomTable extends LitElement {
     this.button.addEventListener('click', () => this.roll())
   }
 
+  get mode(): TableMode {
+    if (this.table.rows[0].cells.length == 2) {
+      return TableMode.TwoColumn
+    } else {
+      return TableMode.FirstColumn
+    }
+  }
+
   private get button(): HTMLButtonElement {
     return this.querySelector('button') as HTMLButtonElement
   }
@@ -26,10 +41,23 @@ export class VellumRandomTable extends LitElement {
     return this.querySelector('table') as HTMLTableElement
   }
 
-  private get selection(): string[] {
+  private get die(): Die | undefined {
+    const maybeDieNotation = this.table.rows[0].cells[0].textContent
+    if (maybeDieNotation) return new Die(maybeDieNotation)
+    return undefined
+  }
+
+  private ranges(column: number): Range[] {
+    return this.selection(column)
+      .map((content) => content.trim())
+      .map((cell) => parseRange(cell))
+      .filter((item): item is Range => !!item)
+  }
+
+  private selection(column: number): string[] {
     return Array.from(this.table.tBodies)
       .flatMap((tbody) => Array.from(tbody.rows))
-      .map((row) => row.cells[0])
+      .map((row) => row.cells[column])
       .map((cell) => cell.textContent)
       .map((content) => (content ? content : ''))
       .map((content) => content.trim())
@@ -41,13 +69,33 @@ export class VellumRandomTable extends LitElement {
   }
 
   roll(): void {
-    if (this.resultTarget) {
-      const selection = this.selection
-      const result = selection[Math.floor(Math.random() * selection.length)]
+    const target = this.resultTarget
+    if (!target) return
 
-      if (this.resultTarget instanceof HTMLInputElement)
-        this.resultTarget.value = result
-      else this.resultTarget.textContent = result
+    if (this.mode == TableMode.FirstColumn) {
+      const selection = this.selection(0)
+      const result = selection[Math.floor(Math.random() * selection.length)]
+      this.display(result)
+    } else if (this.mode == TableMode.TwoColumn) {
+      const ranges = this.ranges(0)
+      const selection = this.selection(1)
+
+      const roll = this.die?.roll()
+
+      if (roll) {
+        const index = ranges.findIndex((range) => range.includes(roll))
+
+        const result = selection[index]
+        this.display(`${result} (${roll})`)
+      }
+    }
+  }
+
+  private display(result: string): void {
+    if (this.resultTarget && this.resultTarget instanceof HTMLInputElement) {
+      this.resultTarget.value = result
+    } else if (this.resultTarget) {
+      this.resultTarget.textContent = result
     }
   }
 
